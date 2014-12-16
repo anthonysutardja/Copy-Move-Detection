391b08fc5845ced5ff102c88adde59224839aae5
kevintee <kevintee@berkeley.edu>
2014-12-15 20:34:18 -0800

    Implement detect copy move, works for non-rotation

--- detect_copy_move.m
+++ detect_copy_move.m
@@ -1,27 +1,18 @@
-function [is_copy_move] = detect_copy_move(im, matches)
+function [is_copy_move] = detect_copy_move(matches)
 % This function takes in a series of points and determines
 % if there is a copy move.
 
 is_copy_move = false;
 
-epsilon = 0.2;
+epsilon = 0.15;
 
-[height, width, ~] = size(im);
-points = matches.target;
-[n, ~] = size(points);
-density = zeros(height, width);
-
-% Plot the points on some image
-for i = 1:n
-    point = points(i,:);
-    x = point(2);
-    y = point(1);
-    density(y,x) = 0.1;
-end
+src_points = matches.source;
+target_points = matches.target;
+[n, ~] = size(target_points);
 
 % Run kmeans to get clusters of points
 k = 2;
-[idx, ~, sumd] = kmeans(points, k);
+[idx, ~, sumd] = kmeans(target_points, k);
 num_points = zeros(k,1);
 
 % Find the number of points in each cluster
@@ -29,19 +20,40 @@ for i = 1:size(idx)
     num_points(idx(i)) = num_points(idx(i))+1;
 end
 
+% Find average distance of each cluster
 for i = 1:k
     sumd(i) = sumd(i) / num_points(i)^2;
 end
 
-% Get the lowest and second lowest
+% Sort the average distances
+% Get the average distance of the first and second lowest
 % If less than epsilon -> copy move
 sort(sumd);
 if sumd(1)/sumd(2) < epsilon
     is_copy_move = true;
 end
 
-disp(num_points);
-disp(sumd);
+% Find distances between corresponding points
+distances = zeros(n,1);
+for i = 1:n
+    src = src_points(i,:);
+    target = target_points(i,:);
+    dist = pdist([src(2) src(1); target(2) target(1)]);
+    distances(i) = dist;
+end
+
+% Here, we bucket the distances
+% Then a copy match occurs if there is a bucket after the first
+% that has over the threshold amount of the total points
+num_buckets = 10;
+threshold = 0.8;
+h = hist(distances, num_buckets);
+for i = 2:size(h,2)
+    if h(i)/n > threshold
+        is_copy_move = true;
+    end
+end
+
 
 end
 
